1a6c7fc1c4acd04a6a2636f584af7521b06f289c
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-09-03 18:40:53 +0300

    new Powerup and soccer game

--- ArcadeSumo/Assets/Scripts/PlayerController.cs
+++ ArcadeSumo/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public PowerUpType currentPowerUp = PowerUpType.None;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
     private Rigidbody rb;
+    public GameObject rocketPrefab;
+    private GameObject tmpRocket;
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
     public float speed = 5.0f;
+    private Coroutine powerupCountdown;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("FocalPoint");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
         float forwardInput = Input.GetAxis("Vertical");
         rb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
+        {
+            LaunchRockets();
+        }
+
+
+
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
+            powerupIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            if (powerupCountdown != null)
+            {
+                StopCoroutine(powerupCountdown);
+            }
+            powerupCountdown = StartCoroutine(PowerupCountdownRoutine());
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp==PowerUpType.Pushback)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Collided with enemy with powerup");
+        }
+    }
+    void LaunchRockets()
+    {
+        foreach (var enemy in FindObjectsOfType<Enemy>())
+        {
+            tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up,
+            Quaternion.identity);
+            tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        currentPowerUp = PowerUpType.None;
+        powerupIndicator.SetActive(false);
+    }
+
 }
--- ArcadeSumo/Assets/Scripts/SpawnManager.cs
+++ ArcadeSumo/Assets/Scripts/SpawnManager.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject[] powerupPrefab;
+    public GameObject[] enemyPrefab;
+    private int powerupIndex;
+    private int enemyIndex;
     private float spawnPosX;
     private float spawnPosZ;
     private float spawnRangeX = 7.0f;
     private float spawnRangeZ = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        powerupIndex = Random.Range(0, powerupPrefab.Length);
+        Instantiate(powerupPrefab[powerupIndex], GenerateSpawnPosition(), powerupPrefab[powerupIndex].transform.rotation);
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            powerupIndex = Random.Range(0, powerupPrefab.Length);
+            Instantiate(powerupPrefab[powerupIndex], GenerateSpawnPosition(), powerupPrefab[powerupIndex].transform.rotation);
+        }
+    }
 
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            enemyIndex = Random.Range(0, enemyPrefab.Length);
+            Instantiate(enemyPrefab[enemyIndex], GenerateSpawnPosition(), enemyPrefab[enemyIndex].transform.rotation);
+        }
     }
     private Vector3 GenerateSpawnPosition()
     {
